89ddcd6fe45e9fc7239d858a641b26a0b2289f2a
BrianDX <boris.zaitsev@devexpress.com>
2019-02-15 18:34:34 +0300

    Update VB

---
+++ VB/CustomFilterPopupSample/CustomizePivotGrid.vb
@@ -0,0 +1,78 @@
+﻿Imports DevExpress.XtraPivotGrid
+Imports DevExpress.XtraPivotGrid.Data
+Imports System.Collections.Generic
+Imports System.Windows.Forms
+Imports DevExpress.Utils
+Imports System.IO
+Imports System.Data
+Imports System
+Namespace CustomFilterPopupSample
+	Partial Public Class CustomFilterPopupForm
+		Private productNameFilterRanges As CustomFilterRanges
+		Private dateFilterRanges As CustomFilterRanges
+		Private Sub CustomizePivotGrid(ByVal pivotGrid As PivotGridControl)
+			pivotGrid.DataSource = CreateCustomDataTable()
+			Const QuantityFieldName As String = "Quantity"
+			Const ProductNameFieldName As String = "ProductName"
+			Const DateFieldName As String = "Date"
+			Dim fieldQuantity As New PivotGridField(QuantityFieldName, PivotArea.DataArea)
+			Dim fieldProductName As New PivotGridField(ProductNameFieldName, PivotArea.RowArea)
+			Dim fieldDate As New PivotGridField(DateFieldName, PivotArea.ColumnArea)
+			fieldDate.GroupInterval = PivotGroupInterval.Date
+			pivotGrid.Fields.AddRange(New PivotGridField() { fieldProductName, fieldQuantity, fieldDate })
+			productNameFilterRanges = New CustomFilterRanges(pivotGrid, fieldProductName)
+			dateFilterRanges = New CustomFilterRanges(pivotGrid, fieldDate)
+		End Sub
+		Private Function CreateCustomDataTable() As DataTable
+			Dim customDataTable As New DataTable("Shopping")
+			customDataTable.Columns.Add("Quantity", GetType(Single))
+			customDataTable.Columns.Add("Price", GetType(Single))
+			customDataTable.Columns.Add("ProductName", GetType(String))
+			customDataTable.Columns.Add("Date", GetType(Date))
+			customDataTable.Rows.Add(5.0, 50.0, Nothing, Date.Today.AddYears(-1).AddMonths(-1))
+			customDataTable.Rows.Add(10.0, 31.0, Nothing, Date.Today.AddYears(-1).AddMonths(-1))
+			customDataTable.Rows.Add(3.0, 30.0, Nothing, Date.Today.AddYears(-1).AddMonths(-1))
+			customDataTable.Rows.Add(3.5, 230.0, "Bananas", Date.Today.AddYears(-1).AddMonths(-1))
+			customDataTable.Rows.Add(5.0, 50.0, "Apples", Date.Today.AddDays(-8))
+			customDataTable.Rows.Add(10.0, 31.0, "Peaches", Date.Today.AddDays(-8))
+			customDataTable.Rows.Add(3.0, 30.0, "Lemons", Date.Today.AddDays(-8))
+			customDataTable.Rows.Add(3.5, 230.0, "Bananas", Date.Today.AddDays(-8))
+			customDataTable.Rows.Add(5.0, 52.0, "Apples", Date.Today.AddDays(-7))
+			customDataTable.Rows.Add(1.0, 33.0, "Peaches", Date.Today.AddDays(-7))
+			customDataTable.Rows.Add(2.0, 32.0, "Lemons", Date.Today.AddDays(-7))
+			customDataTable.Rows.Add(2.3, 250.20, "Bananas", Date.Today.AddDays(-7))
+			customDataTable.Rows.Add(5.0, 50.0, "Apples", Date.Today.AddDays(-6))
+			customDataTable.Rows.Add(10.0, 31.0, "Peaches", Date.Today.AddDays(-6))
+			customDataTable.Rows.Add(3.0, 30.0, "Lemons", Date.Today.AddDays(-6))
+			customDataTable.Rows.Add(3.5, 230.0, "Bananas", Date.Today.AddDays(-6))
+			customDataTable.Rows.Add(5.0, 52.0, "Apples", Date.Today.AddDays(-2))
+			customDataTable.Rows.Add(1.0, 33.0, "Peaches", Date.Today.AddDays(-2))
+			customDataTable.Rows.Add(2.0, 32.0, "Lemons", Date.Today.AddDays(-2))
+			customDataTable.Rows.Add(2.3, 250.20, "Bananas", Date.Today.AddDays(-2))
+			customDataTable.Rows.Add(1.0, 55.0, "Apples", Date.Today.AddDays(-1))
+			customDataTable.Rows.Add(2.0, 38.0, "Peaches", Date.Today.AddDays(-1))
+			customDataTable.Rows.Add(1.0, 36.0, "Lemons", Date.Today.AddDays(-1))
+			customDataTable.Rows.Add(2.0, 55.0, "Apples", Date.Today)
+			customDataTable.Rows.Add(2.0, 39.0, "Peaches", Date.Today)
+			customDataTable.Rows.Add(1.0, 37.0, "Lemons", Date.Today)
+			customDataTable.Rows.Add(3.5, 270.30, "Bananas", Date.Today)
+			customDataTable.Rows.Add(2.0, 55.0, "Apples", Date.Today.AddDays(1))
+			customDataTable.Rows.Add(3.0, 40.5, "Peaches", Date.Today.AddDays(1))
+			customDataTable.Rows.Add(4.2, 290.8, "Bananas", Date.Today.AddDays(1))
+			customDataTable.Rows.Add(10.0, 56.0, "Apples", Date.Today.AddMonths(1))
+			customDataTable.Rows.Add(1.0, 41.0, "Peaches", Date.Today.AddMonths(1))
+			customDataTable.Rows.Add(2.0, 39.0, "Lemons", Date.Today.AddMonths(1))
+			customDataTable.Rows.Add(1.5, 290.30, "Bananas", Date.Today.AddMonths(1))
+			customDataTable.Rows.Add(10.0, 56.0, "Apples", Date.Today.AddMonths(1).AddDays(1))
+			customDataTable.Rows.Add(1.0, 41.0, "Peaches", Date.Today.AddMonths(1).AddDays(1))
+			customDataTable.Rows.Add(2.0, 39.0, "Lemons", Date.Today.AddMonths(1).AddDays(1))
+			customDataTable.Rows.Add(1.5, 290.30, "Bananas", Date.Today.AddMonths(1).AddDays(1))
+			customDataTable.Rows.Add(5.0, 57.2, "Apples", Date.Today.AddYears(1))
+			customDataTable.Rows.Add(3.0, 42.0, "Peaches", Date.Today.AddYears(1))
+			customDataTable.Rows.Add(1.0, 43.0, "Lemons", Date.Today.AddYears(1))
+			customDataTable.Rows.Add(1.2, 295.30, "Bananas", Date.Today.AddYears(1))
+			Return customDataTable
+		End Function
+	End Class
+End Namespace
+
---
+++ VB/CustomFilterPopupSample/Form1.vb
@@ -0,0 +1,27 @@
+﻿Imports DevExpress.XtraEditors
+Imports System
+Imports System.Windows.Forms
+
+Namespace CustomFilterPopupSample
+	Partial Public Class CustomFilterPopupForm
+		Inherits XtraForm
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+		Private Sub CustomFilterPopupForm_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+			CustomizePivotGrid(pivotGrid)
+			pivotGrid.BestFit()
+		End Sub
+
+		Private Sub cbEnableCustomFilterPopup_CheckedChanged(ByVal sender As Object, ByVal e As EventArgs) Handles cbEnableCustomFilterPopup.CheckedChanged
+			If (TryCast(sender, CheckBox)).Checked Then
+				productNameFilterRanges.AddEvents()
+				dateFilterRanges.AddEvents()
+			Else
+				productNameFilterRanges.Detach()
+				dateFilterRanges.Detach()
+			End If
+		End Sub
+	End Class
+End Namespace
